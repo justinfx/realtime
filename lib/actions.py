346db824d18d35605fc0500784ce933e23274053
Justin Israel <justinisrael@gmail.com>
2011-08-23 13:59:15 -0700

    lib/actions.py: fixing PYTHONPATH for non existant key

--- lib/actions.py
+++ lib/actions.py
@@ -9,7 +9,7 @@ CONF = os.path.join(ROOT, "etc/supervisord.conf")
 SUPERD = os.path.join(ROOT, "supervisord")
 SUPERCTL = os.path.join(ROOT, "supervisorctl")
 
-os.environ["PYTHONPATH"] = ':'.join([os.environ["PYTHONPATH"], os.path.join(THIS, "supervisor")])
+os.environ["PYTHONPATH"] = ':'.join([os.environ,get("PYTHONPATH"), os.path.join(THIS, "supervisor")])
 
 
 def start():
